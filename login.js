52eb392c173168eacb7a02e2d6f688fd49e50203
janbin <janbinkimo@gmail.com>
2020-11-06 20:11:24 +0800

    change onreadystagechange content

--- login.js
+++ login.js
@@ -5,12 +5,12 @@ function dataProcess(){
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function(){
 		if (this.readyState == 4 && this.status == 200) {
-			/*if (accountElement[0].value == account_pass.account) {
+			if (accountElement[0].value == account_pass.account) {
 				alert("登入成功");
 			} else {
 				alert("登入失敗");
-			} */
-			document.write(this.responseText);	
+			} 
+			//document.write(this.responseText);	
 		}
 	
 	};
