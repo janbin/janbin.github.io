295e91ce5affa4791b9393d384b89d4201b30116
janbin <janbinkimo@gmail.com>
2020-11-06 18:18:37 +0800

    change for try xmlhttprequest

--- login.js
+++ login.js
@@ -2,12 +2,23 @@
 function dataProcess(){
 	var accountElement = document.getElementsByName("帳號");
 	var passwordElement = document.getElementsByName("密碼");
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function(){
+		if (this.readyState == 4 && this.status == 200) {
+			/*if (accountElement[0].value == account_pass.account) {
+				alert("登入成功");
+			} else {
+				alert("登入失敗");
+			} */
+			document.write(this.responseText);	
+		}
 	
-	if (accountElement[0].value == account_pass.account) {
-		alert("登入成功");
-	} else {
-		alert("登入失敗");
-	}  
+	};
+	xhttp.open("GET", "data.txt", true);
+	xhttp.send();
+	//document.write("function loaded");
+	
+	
 }
   	
 	
